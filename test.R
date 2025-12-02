32b624b580cbce796a751da33c77c03efc21a2c1
dingzishan23-netizen <dingzishan23@gmail.com>
2025-12-02 07:28:51 +0000

    Delete test.R

--- test.R
+++
@@ -1,9 +0,0 @@
-#test file 
-library(tidyverse)
-library(ggplot2)
-library(janitor)
-
-setwd("/Users/lizhen/Desktop/ICY1/Data Science /project")
-continents <- read_csv("continents-according-to-our-world-in-data.csv") %>%
-  clean_names() %>% rename(country = entity, iso3 = code, continent = continent) %>%
-  filter(continent != "Antarctica")
